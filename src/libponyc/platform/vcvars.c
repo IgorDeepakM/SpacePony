5cd178363eaf4da4cff23aa7cbeae8bb20ac0019
IgorDeepakM <117689228+IgorDeepakM@users.noreply.github.com>
2026-02-13 21:59:19 +0100

    Update to LLVM version 19.1.1
    SpacePony now uses LLVM version 19.1.1

--- src/libponyc/platform/vcvars.c
+++ src/libponyc/platform/vcvars.c
@@ -236,6 +236,7 @@ static bool find_kernel32(vcvars_t* vcvars, errors_t* errors)
   vcvars->ucrt[0] = '\0';
   strcpy(vcvars->default_libs,
     "kernel32.lib "
+    "ntdll.lib "
     "msvcrt.lib "
     "Ws2_32.lib advapi32.lib vcruntime.lib "
     "legacy_stdio_definitions.lib dbghelp.lib");
