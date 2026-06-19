ponylang/ponyc

Fix unnecessary reallocations when building small Strings and Arrays (#5518)
String and Array rounded capacity to the next power of two, which records a
capacity smaller than the runtime allocator's minimum block (HEAP_MIN, 32
bytes) for small collections. Because the heap realloc has no same-size-class
fast path, a small collection growing through its first few elements
reallocated and copied several times even though it never left the single
block the allocator handed out. Building a small string by copying a few
characters and then appending hit the same problem.

Add a Pointer[A]._min_alloc() compiler intrinsic and floor capacity with it in
the constructors, reserve, and compact of both String and Array. The copying
String constructors copy only the content, not the floored capacity, so they
do not over-read their source. Format.apply and File.read_string relied on
String(len).space() == len, which the floor breaks; both now use the intended
length. The HEAP_MIN to codegen dependency is documented at its definition and
in the Known Couplings list.

packages/builtin/array.pony
@@ -63,21 +63,25 @@ class Array[A] is Seq[A]
   Different data types require different amounts of memory. Array[U64] with size
   of 6 will take more memory than an Array[U8] of the same size.
 
-  When creating an Array or adding more elements will calculate the next power
-  of 2 of the requested number of elements and allocate that much space, with a
-  lower bound of space for 8 elements.
+  When creating an Array or adding more elements, the space allocated is the
+  next power of 2 of the requested number of elements, with a lower bound: the
+  larger of 8 elements and the number of elements that fill the runtime
+  allocator's minimum block (32 bytes). For small element types the block bound
+  dominates — an `Array[U8]` always has space for at least 32 elements and an
+  `Array[U16]` at least 16 — while for elements of 4 bytes or more the 8-element
+  bound applies.
 
-  Here's a few examples of the space allocated when initialising an Array with
-  various number of elements:
+  Here's a few examples of the space allocated for an `Array[U8]` with various
+  numbers of elements:
 
   | size | space |
   |------|-------|
   | 0    | 0     |
-  | 1    | 8     |
-  | 8    | 8     |
-  | 9    | 16    |
-  | 16   | 16    |
-  | 17   | 32    |
+  | 1    | 32    |
+  | 32   | 32    |
+  | 33   | 64    |
+  | 64   | 64    |
+  | 65   | 128   |
 
   Call the `compact()` method to ask the GC to reclaim unused space. There are
   no guarantees that the GC will actually reclaim any space.
@@ -93,7 +97,7 @@ class Array[A] is Seq[A]
     _size = 0
 
     if len > 0 then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(Pointer[A]._min_alloc())
       _ptr = Pointer[A]._alloc(_alloc)
     else
       _alloc = 0
@@ -107,7 +111,7 @@ class Array[A] is Seq[A]
     _size = len
 
     if len > 0 then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(Pointer[A]._min_alloc())
       _ptr = Pointer[A]._alloc(_alloc)
 
       var i: USize = 0
@@ -174,10 +178,12 @@ class Array[A] is Seq[A]
   fun ref reserve(len: USize) =>
     """
     Reserve space for len elements, including whatever elements are already in
-    the array. Array space grows geometrically.
+    the array. Array space grows geometrically. The space reserved is never
+    smaller than the runtime allocator's minimum block, so for small element
+    types it may exceed len.
     """
     if _alloc < len then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(_ptr._min_alloc())
       _ptr = _ptr._realloc(_alloc, _size)
     end
 
@@ -193,8 +199,9 @@ class Array[A] is Seq[A]
     request may be ignored.
     """
     if _size <= (512 / _ptr._element_size()) then
-      if _size.next_pow2() != _alloc.next_pow2() then
-        _alloc = _size.next_pow2()
+      let target = _size.next_pow2().max(_ptr._min_alloc())
+      if target < _alloc then
+        _alloc = target
         let old_ptr = _ptr = Pointer[A]._alloc(_alloc)
         old_ptr._copy_to(_ptr._convert[A!](), _size)
       end

packages/builtin/pointer.pony
@@ -64,6 +64,17 @@ struct Pointer[A]
     """
     compile_intrinsic
 
+  fun tag _min_alloc(): USize =>
+    """
+    Return how many elements fill the runtime allocator's minimum block
+    (`HEAP_MIN / element_size`), but always at least one. A smaller allocation is
+    rounded up to the same block by the allocator anyway, so String and Array
+    use this as a capacity floor (in their constructors, reserve, and compact)
+    to avoid recording less space than the block actually provides (which would
+    force pointless reallocations as the collection grows).
+    """
+    compile_intrinsic
+
   fun ref _insert(n: USize, len: USize): Pointer[A] =>
     """
     Creates space for n new elements at the head, moving following elements.

packages/builtin/string.pony
@@ -56,7 +56,7 @@ actor Main
     An empty string. Enough space for len bytes is reserved.
     """
     _size = 0
-    _alloc = len.min(len.max_value() - 1) + 1
+    _alloc = (len.min(len.max_value() - 1) + 1).max(Pointer[U8]._min_alloc())
     _ptr = Pointer[U8]._alloc(_alloc)
     _set(0, 0)
 
@@ -131,14 +131,15 @@ actor Main
     """
     if str.is_null() then
       _size = 0
-      _alloc = 1
+      _alloc = Pointer[U8]._min_alloc()
       _ptr = Pointer[U8]._alloc(_alloc)
       _set(0, 0)
     else
       _size = len
-      _alloc = _size + 1
+      _alloc = (_size + 1).max(Pointer[U8]._min_alloc())
       _ptr = Pointer[U8]._alloc(_alloc)
-      str._copy_to(_ptr, _alloc)
+      // copy only the content (plus terminator slot); _alloc may be larger.
+      str._copy_to(_ptr, _size + 1)
     end
 
   new copy_cstring(str: Pointer[U8] box) =>
@@ -150,7 +151,7 @@ actor Main
     """
     if str.is_null() then
       _size = 0
-      _alloc = 1
+      _alloc = Pointer[U8]._min_alloc()
       _ptr = Pointer[U8]._alloc(_alloc)
       _set(0, 0)
     else
@@ -161,9 +162,10 @@ actor Main
       end
 
       _size = i
-      _alloc = i + 1
+      _alloc = (i + 1).max(Pointer[U8]._min_alloc())
       _ptr = Pointer[U8]._alloc(_alloc)
-      str._copy_to(_ptr, _alloc)
+      // copy only the content (plus terminator slot); _alloc may be larger.
+      str._copy_to(_ptr, i + 1)
     end
 
   new from_utf32(value: U32) =>
@@ -172,7 +174,7 @@ actor Main
     """
     let encoded = _UTF32Encoder.encode(value)
     _size = encoded._1
-    _alloc = _size + 1
+    _alloc = (_size + 1).max(Pointer[U8]._min_alloc())
     _ptr = Pointer[U8]._alloc(_alloc)
     _set(0, encoded._2)
     if encoded._1 > 1 then
@@ -290,13 +292,15 @@ actor Main
   fun ref reserve(len: USize) =>
     """
     Reserve space for len bytes. An additional byte will be reserved for the
-    null terminator.
+    null terminator. The space reserved is never smaller than the runtime
+    allocator's minimum block (32 bytes), so a small string may reserve more
+    than requested.
     """
     if _alloc <= len then
       let max = len.max_value() - 1
       let min_alloc = len.min(max) + 1
       if min_alloc <= (max / 2) then
-        _alloc = min_alloc.next_pow2()
+        _alloc = min_alloc.next_pow2().max(_ptr._min_alloc())
       else
         _alloc = min_alloc.min(max)
       end
@@ -309,8 +313,9 @@ actor Main
     request may be ignored. The string is returned to allow call chaining.
     """
     if (_size + 1) <= 512 then
-      if (_size + 1).next_pow2() != _alloc.next_pow2() then
-        _alloc = (_size + 1).next_pow2()
+      let target = (_size + 1).next_pow2().max(_ptr._min_alloc())
+      if target < _alloc then
+        _alloc = target
         let old_ptr = _ptr = Pointer[U8]._alloc(_alloc)
         old_ptr._copy_to(_ptr, _size)
         _set(_size, 0)

IgorDeepakM/SpacePony

packages/builtin/array.pony
@@ -63,21 +63,25 @@ class Array[\allowbaretypes, noconstraintcheck\ A] is Seq[A]
   Different data types require different amounts of memory. Array[U64] with size
   of 6 will take more memory than an Array[U8] of the same size.
 
-  When creating an Array or adding more elements will calculate the next power
-  of 2 of the requested number of elements and allocate that much space, with a
-  lower bound of space for 8 elements.
+  When creating an Array or adding more elements, the space allocated is the
+  next power of 2 of the requested number of elements, with a lower bound: the
+  larger of 8 elements and the number of elements that fill the runtime
+  allocator's minimum block (32 bytes). For small element types the block bound
+  dominates — an `Array[U8]` always has space for at least 32 elements and an
+  `Array[U16]` at least 16 — while for elements of 4 bytes or more the 8-element
+  bound applies.
 
-  Here's a few examples of the space allocated when initialising an Array with
-  various number of elements:
+  Here's a few examples of the space allocated for an `Array[U8]` with various
+  numbers of elements:
 
   | size | space |
   |------|-------|
   | 0    | 0     |
-  | 1    | 8     |
-  | 8    | 8     |
-  | 9    | 16    |
-  | 16   | 16    |
-  | 17   | 32    |
+  | 1    | 32    |
+  | 32   | 32    |
+  | 33   | 64    |
+  | 64   | 64    |
+  | 65   | 128   |
 
   Call the `compact()` method to ask the GC to reclaim unused space. There are
   no guarantees that the GC will actually reclaim any space.
@@ -93,7 +97,7 @@ class Array[\allowbaretypes, noconstraintcheck\ A] is Seq[A]
     _size = 0
 
     if len > 0 then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(Pointer[A].min_alloc())
       _ptr = Pointer[A].alloc(_alloc)
     else
       _alloc = 0
@@ -107,7 +111,7 @@ class Array[\allowbaretypes, noconstraintcheck\ A] is Seq[A]
     _size = len
 
     if len > 0 then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(Pointer[A].min_alloc())
       _ptr = Pointer[A].alloc(_alloc)
 
       var i: USize = 0
@@ -174,10 +178,12 @@ class Array[\allowbaretypes, noconstraintcheck\ A] is Seq[A]
   fun ref reserve(len: USize) =>
     """
     Reserve space for len elements, including whatever elements are already in
-    the array. Array space grows geometrically.
+    the array. Array space grows geometrically. The space reserved is never
+    smaller than the runtime allocator's minimum block, so for small element
+    types it may exceed len.
     """
     if _alloc < len then
-      _alloc = len.next_pow2().max(len).max(8)
+      _alloc = len.next_pow2().max(len).max(8).max(_ptr.min_alloc())
       _ptr = _ptr.realloc(_alloc, _size)
     end
 
@@ -193,8 +199,9 @@ class Array[\allowbaretypes, noconstraintcheck\ A] is Seq[A]
     request may be ignored.
     """
     if _size <= (512 / _ptr._element_size()) then
-      if _size.next_pow2() != _alloc.next_pow2() then
-        _alloc = _size.next_pow2()
+      let target = _size.next_pow2().max(_ptr.min_alloc())
+      if target < _alloc then
+        _alloc = target
         let old_ptr = _ptr = Pointer[A].alloc(_alloc)
         old_ptr._copy_to(_ptr.convert[A!](), _size)
       end

packages/builtin/pointer.pony
@@ -82,6 +82,17 @@ struct Pointer[\allowbaretypes, noconstraintcheck\ A]
     """
     compile_intrinsic
 
+  fun tag min_alloc(): USize =>
+    """
+    Return how many elements fill the runtime allocator's minimum block
+    (`HEAP_MIN / element_size`), but always at least one. A smaller allocation is
+    rounded up to the same block by the allocator anyway, so String and Array
+    use this as a capacity floor (in their constructors, reserve, and compact)
+    to avoid recording less space than the block actually provides (which would
+    force pointless reallocations as the collection grows).
+    """
+    compile_intrinsic
+
   fun ref _insert(n: USize, len: USize): Pointer[A] =>
     """
     Creates space for n new elements at the head, moving following elements.

packages/builtin/string.pony
@@ -56,7 +56,7 @@ actor Main
     An empty string. Enough space for len bytes is reserved.
     """
     _size = 0
-    _alloc = len.min(len.max_value() - 1) + 1
+    _alloc = (len.min(len.max_value() - 1) + 1).max(Pointer[U8].min_alloc())
     _ptr = Pointer[U8].alloc(_alloc)
     _set(0, 0)
 
@@ -131,14 +131,15 @@ actor Main
     """
     if str.is_null() then
       _size = 0
-      _alloc = 1
+      _alloc = Pointer[U8].min_alloc()
       _ptr = Pointer[U8].alloc(_alloc)
       _set(0, 0)
     else
       _size = len
-      _alloc = _size + 1
+      _alloc = (_size + 1).max(Pointer[U8].min_alloc())
       _ptr = Pointer[U8].alloc(_alloc)
-      str._copy_to(_ptr, _alloc)
+      // copy only the content (plus terminator slot); _alloc may be larger.
+      str._copy_to(_ptr, _size + 1)
     end
 
   new copy_cstring(str: Pointer[U8] box) =>
@@ -150,7 +151,7 @@ actor Main
     """
     if str.is_null() then
       _size = 0
-      _alloc = 1
+      _alloc = Pointer[U8].min_alloc()
       _ptr = Pointer[U8].alloc(_alloc)
       _set(0, 0)
     else
@@ -161,9 +162,11 @@ actor Main
       end
 
       _size = i
-      _alloc = i + 1
+
+      _alloc = (i + 1).max(Pointer[U8].min_alloc())
       _ptr = Pointer[U8].alloc(_alloc)
-      str._copy_to(_ptr, _alloc)
+      // copy only the content (plus terminator slot); _alloc may be larger.
+      str._copy_to(_ptr, i + 1)
     end
 
   new from_utf32(value: U32) =>
@@ -172,7 +175,7 @@ actor Main
     """
     let encoded = _UTF32Encoder.encode(value)
     _size = encoded._1
-    _alloc = _size + 1
+    _alloc = (_size + 1).max(Pointer[U8].min_alloc())
     _ptr = Pointer[U8].alloc(_alloc)
     _set(0, encoded._2)
     if encoded._1 > 1 then
@@ -290,13 +293,15 @@ actor Main
   fun ref reserve(len: USize) =>
     """
     Reserve space for len bytes. An additional byte will be reserved for the
-    null terminator.
+    null terminator. The space reserved is never smaller than the runtime
+    allocator's minimum block (32 bytes), so a small string may reserve more
+    than requested.
     """
     if _alloc <= len then
       let max = len.max_value() - 1
       let min_alloc = len.min(max) + 1
       if min_alloc <= (max / 2) then
-        _alloc = min_alloc.next_pow2()
+        _alloc = min_alloc.next_pow2().max(_ptr.min_alloc())
       else
         _alloc = min_alloc.min(max)
       end
@@ -309,8 +314,9 @@ actor Main
     request may be ignored. The string is returned to allow call chaining.
     """
     if (_size + 1) <= 512 then
-      if (_size + 1).next_pow2() != _alloc.next_pow2() then
-        _alloc = (_size + 1).next_pow2()
+      let target = (_size + 1).next_pow2().max(_ptr.min_alloc())
+      if target < _alloc then
+        _alloc = target
         let old_ptr = _ptr = Pointer[U8].alloc(_alloc)
         old_ptr._copy_to(_ptr, _size)
         _set(_size, 0)